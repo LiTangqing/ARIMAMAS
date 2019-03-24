d8849bddda5e0db3a450f18b7d11eca14aac0e7c
Li Tangqing <tangqinglucy@gmail.com>
2019-03-24 20:26:52 +0800

    incorparte volume as predictors & retrained the model

--- LSTMKeras.py
+++ LSTMKeras.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pandas as pd
 #import matplotlib.pyplot as plt
-
- 
 from keras.models import Sequential
 from keras.layers import Activation, Dense, Input, Reshape, Flatten,TimeDistributed
 from keras.layers import LSTM, Dropout
@@ -11,58 +9,64 @@ from keras.layers.pooling import MaxPooling1D
 from keras.layers import ConvLSTM2D
 
 
-def createAndTrain(DATE, CLOSE, settings):  
+def createAndTrain(DATE, CLOSE, VOL, settings):  
     _TS = 1 # timestamp  
-
     # read dates
-    dates = DATE.tolist()[_TS:]
+    # nMarkets=CLOSE.shape[1]
+    # print('num of markets:', nMarkets)
     
-    # normalise price data  
-    average = np.average(CLOSE[:-_TS, :], axis=0)
-    std_dev = np.std(CLOSE[:-_TS, :], axis=0)
-    prices = (CLOSE[:-_TS, :] - average) / std_dev
-    # prices = normalize(CLOSE[:-_TS, :])
-     
-    returns = (CLOSE[_TS:, :] - CLOSE[:-_TS, :]) / CLOSE[:-_TS, :] 
+    # print(CLOSE)
+    # print('CLOSE SHAPE:', CLOSE.shape)
+    # print("IS NAN: ", sum(np.isnan(CLOSE)))
+    # normalise price data 
+
+    prices = CLOSE[:-1, :]
+    average = np.average(prices)
+    std_dev = np.std(prices)
+    prices = (prices - average) / std_dev
+
+    print('PRICES SHAPE:', prices.shape)
+    returns = (CLOSE[1:, :] - CLOSE[:-1, :]) / CLOSE[:-1, :] 
+
+    volumes = VOL[:-1, :]
+    average_v = np.average(volumes)
+    sd_v = np.std(volumes)
+    volumes = (volumes - average_v) / sd_v
+
     #return_data = CLOSE[:CLOSE.size-1]
     #return_data = (return_data - average) / std_dev 
-
-    X = np.reshape(prices, (prices.shape[0], _TS, prices.shape[1]))
-    ##########
+    print('RETURNS SHAPE:', returns.shape)
     
+    X = np.hstack((prices, volumes))
+    X = X.reshape(X.shape[0], _TS, X.shape[1])
+
+    # prices.reshape(prices.shape[0], _TS, prices.shape[1])
     y = np.reshape(returns, (returns.shape[0], _TS, returns.shape[1]))
     
-
-    dim_in = X.shape[2]
     dim_out = y.shape[2] # same as number of market
 
     y = returns
     dim_out = y.shape[1]
-    
-    kernel_size = 5
-    filters = 64
-    pool_size = 4
-
+     
     # define model here
     model = Sequential()
      
     model.add(LSTM(units=20, input_shape=(_TS, X.shape[2]),
-                   return_sequences=True, dropout=0.4))
+                   return_sequences=True, dropout=0.5))
+    model.add(LSTM(units=20, input_shape=(_TS, X.shape[2]),
+                   return_sequences=True, dropout=0.5))
     #model.add(Conv1D(filters=10, kernel_size=3, padding='same', activation='relu'))
     #model.add(MaxPooling1D(pool_size=2))
-    model.add(LSTM(10, dropout=0.4)) 
+    model.add(LSTM(10, dropout=0.5)) 
+    model.add(Dense(units=336))
     model.add(Dense(units=dim_out))
     model.add(Activation('linear'))
-
     #model.add(TimeDistributed(Dense(units=dim_out)))
     #model.add(Activation('linear'))
 
     model.compile(loss='mse', optimizer='adam')
-    
     print(model.summary())
-    
-    
-    model.fit(X, y, epochs=300, batch_size=50, verbose=2)
+    model.fit(X, y, epochs=300, batch_size=32, verbose=2)
 
     # plot hist
     # plt.plot(history.history['loss'])
@@ -78,20 +82,35 @@ def createAndTrain(DATE, CLOSE, settings):
     settings['mean'] = average
     settings['std'] = std_dev
     settings['model'] = model
+    settings['v_mean'] = average_v
+    settings['v_sd'] = sd_v
     return
 
 
-def myTradingSystem(DATE, CLOSE, exposure, equity, settings):
+def myTradingSystem(DATE, CLOSE, VOL, USA_ADP, USA_EARN, USA_HRS, 
+                    USA_BOT, USA_BC, USA_BI, USA_CU, USA_CF, USA_CHJC, 
+                    USA_CFNAI, USA_CP, USA_CCR, USA_CPI, USA_CCPI, 
+                    USA_CINF, USA_DFMI, USA_DUR, USA_DURET, USA_EXPX, 
+                    USA_EXVOL, USA_FRET, USA_FBI, USA_GBVL, USA_GPAY, 
+                    USA_HI, USA_IMPX, USA_IMVOL, USA_IP, USA_IPMOM, USA_CPIC, 
+                    USA_CPICM, USA_JBO, USA_LFPR, USA_LEI, USA_MPAY, USA_MP, 
+                    USA_NAHB, USA_NLTTF, USA_NFIB, USA_NFP, USA_NMPMI, USA_NPP, 
+                    USA_EMPST, USA_PHS, USA_PFED, USA_PP, USA_PPIC, USA_RSM, 
+                    USA_RSY, USA_RSEA, USA_RFMI, USA_TVS, USA_UNR, USA_WINV,
+                    exposure, equity, settings):
+
     ''' This system uses mean reversion techniques to allocate capital into the desired equities '''
     lookBack = settings['lookback']
     if 'model' not in settings:
-        createAndTrain(DATE[:lookBack - 2], CLOSE[:lookBack - 2,1:], settings)
+        createAndTrain(DATE[:lookBack - 2], CLOSE[:lookBack - 2,1:], VOL[:lookBack - 2,1:], settings)
 
     model = settings['model']
     average = settings['mean']
     std_dev = settings['std']
+    average_v = settings['v_mean'] 
+    sd_v = settings['v_sd'] 
 
-    testX = (CLOSE[lookBack-1,1:] - average) / std_dev
+    testX = np.hstack(( (CLOSE[lookBack-1,1:] - average) / std_dev, (VOL[lookBack-1,1:] - average_v) / sd_v))
     testX = np.reshape(testX, (1, 1, testX.shape[0]))
     testY = model.predict(testX)
     predicted_returns = testY[0].flatten()
@@ -118,28 +137,29 @@ def mySettings():
     settings = {}
 
     # Futures Contracts
-    settings['markets'] = ['CASH','F_AD','F_BO','F_BP','F_C','F_CC','F_CD',
-                           'F_CL','F_CT','F_DX','F_EC','F_ED','F_ES',
-                           'F_FC','F_FV','F_GC','F_HG','F_HO','F_JY',
-                           'F_KC','F_LB','F_LC','F_LN','F_MD','F_MP',
-                           'F_NG','F_NQ','F_NR','F_O','F_OJ','F_PA',
-                           'F_PL','F_RB','F_RU','F_S','F_SB','F_SF',
-                           'F_SI','F_SM','F_TU','F_TY','F_US','F_W',
-                           'F_XX','F_YM','F_AX','F_CA','F_DT','F_UB',
-                           'F_UZ','F_GS','F_LX','F_SS','F_DL','F_ZQ',
-                           'F_VX','F_AE','F_BG','F_BC','F_LU','F_DM',
-                           'F_AH','F_CF','F_DZ','F_FB','F_FL','F_FM',
-                           'F_FP','F_FY','F_GX','F_HP','F_LR','F_LQ',
-                           'F_ND','F_NY','F_PQ','F_RR','F_RF','F_RP',
-                           'F_RY','F_SH','F_SX','F_TR','F_EB','F_VF',
-                           'F_VT','F_VW','F_GD','F_F']
+    settings['markets'] = ['CASH','F_AD','F_BO','F_BP','F_C','F_CC',
+                           'F_CD','F_CL','F_CT','F_DX','F_EC','F_ED',
+                           'F_ES','F_FC','F_FV','F_GC','F_HG','F_HO',
+                           'F_JY','F_KC','F_LB','F_LC','F_LN','F_MD',
+                           'F_MP','F_NG','F_NQ','F_NR','F_O','F_OJ',
+                           'F_PA','F_PL','F_RB','F_RU','F_S','F_SB',
+                           'F_SF','F_SI','F_SM','F_TU','F_TY','F_US',
+                           'F_W','F_XX','F_YM','F_AX','F_CA','F_DT',
+                           'F_UB','F_UZ','F_GS','F_LX','F_SS','F_DL',
+                           'F_ZQ','F_VX','F_AE','F_BG','F_BC','F_LU',
+                           'F_DM','F_AH','F_CF','F_DZ','F_FB','F_FL',
+                           'F_FM','F_FP','F_FY','F_GX','F_HP','F_LR',
+                           'F_LQ','F_ND','F_NY','F_PQ','F_RR','F_RF',
+                           'F_RP','F_RY','F_SH','F_SX','F_TR','F_EB',
+                           'F_VF','F_VT','F_VW','F_GD','F_F']
+
+    settings['beginInSample'] = '20090506'
+    settings['endInSample'] = '20180101'
 
     settings['slippage'] = 0.05
     settings['budget'] = 10 ** 6
     settings['lookback'] = 504
-    settings['beginInSample'] = '20100101'
-    settings['endInSample'] = '20181231'
-
+    
     return settings
 
 
